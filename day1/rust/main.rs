d8329df2fe2cecc3426529f3955d75ad3a59d422
Oriol <oornaqbl13@alumnes.ub.edu>
2020-12-07 14:19:18 +0100

    day1 solutions with rust

--- day1/rust/main.rs
+++ day1/rust/main.rs
@@ -1,13 +1,9 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() -> std::io::Result<()> {
-
-    let file = File::open("../input")?;
-    let reader = BufReader::new(file);
-
-    let entries: Vec<i32> = reader.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
-
+fn solve1(entries: &Vec<i32>) {
+    // O(n^2)
+    // Could be improved to O(n logn)
     'outer: for num in entries.iter() {
         for num2 in entries.iter() {
             if num + num2 == 2020 {
@@ -16,6 +12,33 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
+}
+
+fn solve2(entries: &Vec<i32>) {
+    // O(n^3)
+    // Could be improved to O(n^2 logn)
+    'outer: for num in entries.iter() {
+        for num2 in entries.iter() {
+            for num3 in entries.iter() {
+                if num + num2 + num3 == 2020 {
+                    println!("{}", num * num2 * num3);
+                    break 'outer;
+                }
+            }
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+
+    let file = File::open("../input")?;
+    let reader = BufReader::new(file);
+
+    let entries: Vec<i32> = reader.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
+
+    solve1(&entries);
+
+    solve2(&entries);
 
     Ok(())
 }
