a7390603f6918d6e643c336968cb98512b1bc57f
Oriol <oornaqbl13@alumnes.ub.edu>
2020-12-17 00:24:41 +0100

    improved rust day1

--- day1/rust/main.rs
+++ day1/rust/main.rs
@@ -1,6 +1,28 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+fn solve1_improved(entries: &Vec<i32>) {
+    // O(n log(n))
+    'outer: for num in entries.iter() {
+        if entries.binary_search(&(2020 - num)).is_ok() {
+            println!("{}", num * (2020 - num));
+            break 'outer;
+        }
+    }
+}
+
+fn solve2_improved(entries: &Vec<i32>) {
+    // O(n^2 logn)
+    'outer: for num in entries.iter() {
+        for num2 in entries.iter() {
+            if entries.binary_search(&(2020 - num - num2)).is_ok() {
+                println!("{}", num * num2 * (2020 - num - num2));
+                break 'outer;
+            }
+        }
+    }
+}
+
 fn solve1(entries: &Vec<i32>) {
     // O(n^2)
     // Could be improved to O(n logn)
@@ -34,11 +56,12 @@ fn main() -> std::io::Result<()> {
     let file = File::open("../input")?;
     let reader = BufReader::new(file);
 
-    let entries: Vec<i32> = reader.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
+    let mut entries: Vec<i32> = reader.lines().map(|line| line.unwrap().parse::<i32>().unwrap()).collect();
 
-    solve1(&entries);
+    entries.sort(); // nlogn
 
-    solve2(&entries);
+    solve1_improved(&entries);
+    solve2_improved(&entries);
 
     Ok(())
 }
